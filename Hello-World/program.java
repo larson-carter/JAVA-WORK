508a689963596f80db3cb33e2648054338efb84f
Larson Carter <larsoncarter@blazermail.net>
2018-03-26 18:59:22 -0500

    Create program.java

---
+++ Hello-World/program.java
@@ -0,0 +1,11 @@
+public class program {
+
+    public static void main(String[] args) {
+    
+        // Prints "Hello, World!" to the terminal/console window.
+        
+        System.out.println("Hello, World!");
+        
+    }
+
+}
