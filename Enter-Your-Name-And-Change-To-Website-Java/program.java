6788d8df40fa424596f27bdc6dc7e96bc70912a4
Larson Carter <larsoncarter@blazermail.net>
2018-03-26 19:00:24 -0500

    Create program.java

---
+++ Enter-Your-Name-And-Change-To-Website-Java/program.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+
+/**
+ * @author 		Larson Carter
+ *
+ * @created		1/17/2018
+ * 
+ * @updated		???
+ *
+ */
+
+import java.util.Scanner;
+
+public class program {
+
+	/**
+	 * 
+	 */
+	public program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+	
+		 //	This Will Set The "Tag" input1
+		 Scanner input1 = new Scanner(System.in);
+		 
+		 //	This Will Ask For Your Name
+		 System.out.println("Enter Your Name : ");
+		 
+		 //	This Will Let The User Input Their Name Using The "Tag" input1 also using "Scanner"
+		 String name = input1.next();
+		
+		 //	This Will Say "Well, Hello"
+		 System.out.print("Well, Hello ");
+		 
+		 //	This Will Print Out The "Tag" "input1" or "name"
+		 System.out.println(name);
+		 
+		 //	This Will Delay The System By 1 Seconds
+		 Thread.sleep(1000);
+		 
+		 //	This Will Say You Are Accessing.
+		 System.out.println("You are accessing the Valley View Food Pantry.");
+		 
+		 //	This Will Delay The System By .575 Seconds
+		 Thread.sleep(0575);
+		 
+		 //	This Will Print The Welcome Section
+		 System.out.print("W");
+		 Thread.sleep(0575);
+		 System.out.print("e");
+		 Thread.sleep(0575);
+		 System.out.print("l");
+		 Thread.sleep(0575);
+		 System.out.print("c");
+		 Thread.sleep(0575);
+		 System.out.print("o");
+		 Thread.sleep(0575);
+		 System.out.print("m");
+		 Thread.sleep(0575);
+		 System.out.println("e");
+		 Thread.sleep(0575);
+	}
+
+}
