85a5e6a03869b4fd6fd68dadfeb3d59725f0e06b
Larson Carter <larsoncarter@blazermail.net>
2018-03-27 09:27:27 -0500

    Create program.java

---
+++ PRINT-USER-NAME/program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class program {
+
+    public static void main(String args[]) {
+        
+        System.out.println("What Is Your Name?");
+
+        Scanner input1 = new Scanner(System.in);
+
+        System.out.println("Well Hello There," + input1);
+        
+    }
+    
+}
