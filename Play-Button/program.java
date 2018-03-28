7f620c3057310226bc63edfd1730c8233a9d0db0
Larson Carter <larsoncarter@blazermail.net>
2018-03-28 11:56:08 -0500

    Create program.java

---
+++ Play-Button/program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class program {
+    public program() {
+}
+    public static void main(String args[]) {
+    
+        int size;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("How Big Do You Want Your Play Button?");
+    
+    }
+}
