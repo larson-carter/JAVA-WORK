54a572d1fbbaa031cf17f5b69f933afd392151f5
Larson Carter <larsoncarter@blazermail.net>
2018-04-16 08:06:36 -0500

    Update program.java

--- 1102/program.java
+++ 1102/program.java
@@ -1,37 +1,46 @@
 import java.util.Scanner;
 public class program {
 	public static void main(String[] args) {
-		//	Set Up Variables
+		// Set Up Variables
 		Scanner sc = new Scanner(System.in);
-		int input, max;
+		int input, max, min;
 		int[] list;
-		//	Read In Values
+		// Read In Values
 		System.out.println("How Many Numbers Are There?");
 		input = sc.nextInt();
-		list = new int [input];
-		for(int i=0; i<list.length; i++) {
+		list = new int[input];
+		for (int i = 0; i < list.length; i++) {
 			System.out.println("Enter An Integer:");
 			list[i] = sc.nextInt();
 		}
-		//	Find The Maximum
+		// Find The Maximum
 		max = maximum(list);
-		//	Prints Our Results To Screen.
+		// Find The Minimum
+		min = minimum(list);
+		// Prints Our Results To Screen
 		System.out.println("The Largest Number Is " + max);
+		System.out.println("The Smallest Number Is " + min);
 	}
-	//	Method to Find The Biggest Integer In A List
-	public static int maximum(int [] list) {
+	// Method to Find The Biggest Integer In A List
+	public static int maximum(int[] list) {
 		int largest;
 		largest = list[0];
-		for(int i=0; i<list.length; i++) {
-			if(list[i] > largest) {
+		for (int i = 0; i < list.length; i++) {
+			if (list[i] > largest) {
 				largest = list[i];
 			}
-		}		
-		return largest;		
+		}
+		return largest;
 	}
-	//	Method To Double An Integer Value
-	public static int doubleInt(int i) {
-		i = i * 2;
-		return i;
+	// Method To Find The Biggest Integer In A List
+	public static int minimum(int[] list) {
+		int smallest;
+		smallest = list[0];
+		for (int i = 1; i < list.length; i++) {
+			if (list[i] < smallest) {
+				smallest = list[i];
+			}
+		}
+		return smallest;
 	}
 }
