ee58948475773a26b674bb296fb2e81816acd516
Larson Carter <larsoncarter@blazermail.net>
2018-05-02 08:31:07 -0500

    Update program.java

--- Coupon-Code/program.java
+++ Coupon-Code/program.java
@@ -1,53 +1,32 @@
 import java.util.Scanner;
-
 public class program {
-
 	public static void main(String[] args) {
-
 		int[] usedCodes = {};
-
 		Scanner sc = new Scanner(System.in);
-
 		int input = 0;
-
 		System.out.print("Enter a coupon code:  ");
-
 		input = sc.nextInt();
-
 		do {
-
 			if (couponIsValid(usedCodes, input)) {
-
 				System.out.println("This is a valid coupon.");
-
-				addToList();
-
-			}
-
-			else {
-
+				int[] moreNums = new int[usedCodes.length + 1];
+				for (int i = 0; i < usedCodes.length; i++) {
+					moreNums[i] = usedCodes[i];
+				}
+				moreNums[moreNums.length - 1] = input;
+				usedCodes = moreNums;
+			} else {
 				System.out.println("This coupon has already been redeemed.");
-
 			}
-
 			System.out.print("Enter another coupon code:  ");
-
 			input = sc.nextInt();
-
 		} while (input >= 0);
-
 	}
-
-	public static void addToList(int input, int usedCodes) {
-		int[] usedCodes.add(input);
-	}
-
 	public static boolean couponIsValid(int[] usedCodes, int input) {
-
-		// Check to see if the input value is already in the usedCodes list.
-
-		return true; // Or false
-
+		for (int i = 0; i < usedCodes.length; i++) {
+			if (usedCodes[i] == input)
+				return false;
+		}
+		return true;
 	}
-
 }
