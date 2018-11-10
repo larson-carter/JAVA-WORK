8c06ceca13b454ec197450b3dbf656be50a6d245
Larson Carter <larsoncarter@blazermail.net>
2018-11-10 12:19:50 -0600

    Create main.java

---
+++ romeo-and-juilet/main.java
@@ -0,0 +1,74 @@
+public class main
+    {
+    public static void main(String [] args)
+        {
+            iLoveYouRomeo.main();   //  This shows That She Can Love Romeo
+            iLoveYouBlank.main();   //  This shows That She Can Love Other People
+            whatIsFate.main();      //  This shows What Romeo and Juilet's Fate Could Be
+
+        }
+    }
+class iLoveYouRomeo
+    {
+    public static void main()
+        {
+        System.out.println("  _____   _                                      _____\n" +
+                " |_   _| | |                                    |  __ \\\n" +
+                "   | |   | | _____   _____   _   _  ___  _   _  | |__) |___  _ __ ___   ___  ___\n" +
+                "   | |   | |/ _ \\ \\ / / _ \\ | | | |/ _ \\| | | | |  _  // _ \\| '_ ` _ \\ / _ \\/ _ \\\n" +
+                "  _| |_  | | (_) \\ V /  __/ | |_| | (_) | |_| | | | \\ \\ (_) | | | | | |  __/ (_) |\n" +
+                " |_____| |_|\\___/ \\_/ \\___|  \\__, |\\___/ \\__,_| |_|  \\_\\___/|_| |_| |_|\\___|\\___/\n" +
+                "                              __/ |\n" +
+                "                             |___/");
+
+        }
+    }
+class iLoveYouBlank
+    {
+    public static void main()
+        {
+        System.out.println("  _____   _                                    \n" +
+                " |_   _| | |                                   \n" +
+                "   | |   | | _____   _____   _   _  ___  _   _ \n" +
+                "   | |   | |/ _ \\ \\ / / _ \\ | | | |/ _ \\| | | |\n" +
+                "  _| |_  | | (_) \\ V /  __/ | |_| | (_) | |_| |\n" +
+                " |_____| |_|\\___/ \\_/ \\___|  \\__, |\\___/ \\__,_|\n" +
+                "                              __/ |            \n" +
+                "                             |___/             ");
+
+        }
+    }
+class whatIsFate
+    {
+     public static void main()
+        {
+            System.out.println("FATE: Juilet could have chosen anyone to love but yet\n" +
+                    "she choose to love Romeo, which led to their tragic\n" +
+                    "death/downfall. In this program above it shows how\n" +
+                    "Romeo could have been easily taken out or put into\n" +
+                    "the picture within just a few seconds.");
+        }
+
+    }
+/*
+  I used the "BIG" FONT
+  _____   _                                      _____
+ |_   _| | |                                    |  __ \
+   | |   | | _____   _____   _   _  ___  _   _  | |__) |___  _ __ ___   ___  ___
+   | |   | |/ _ \ \ / / _ \ | | | |/ _ \| | | | |  _  // _ \| '_ ` _ \ / _ \/ _ \
+  _| |_  | | (_) \ V /  __/ | |_| | (_) | |_| | | | \ \ (_) | | | | | |  __/ (_) |
+ |_____| |_|\___/ \_/ \___|  \__, |\___/ \__,_| |_|  \_\___/|_| |_| |_|\___|\___/
+                              __/ |
+                             |___/
+ */
+/*
+FATE: Juilet could have chosen anyone to love but yet
+she choose to love Romeo, which led to their tragic
+death/downfall. In this program above it shows how
+Romeo could have been easily taken out or put into
+the picture within just a few seconds. It also brings
+curiosity to the table when Tybalt dies, and Mercutio
+puts a curse on both of the houses. As along with all
+of the other bad decisions that Romeo and Juilet make
+throughout the entire story.
+ */
