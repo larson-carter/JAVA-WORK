7bfbda99919a99a821c6f42c04dc7662f0e2f61d
Larson Carter <larsoncarter@blazermail.net>
2018-11-10 12:40:54 -0600

    Update main.java

--- romeo-and-juilet/main.java
+++ romeo-and-juilet/main.java
@@ -1,16 +1,16 @@
 public class main
-    {
+{
     public static void main(String [] args)
-        {
-            iLoveYouRomeo.main();   //  This shows That She Can Love Romeo
-            iLoveYouQuestion.main();   //  This shows That She Can Love Other People
-            whatIsFate.main();      //  This shows What Romeo and Juilet's Fate Could Be
-        }
-    }
-class iLoveYouRomeo
     {
+        iLoveYouRomeo.main();   //  This shows That She Can Love Romeo
+        iLoveYouQuestion.main();   //  This shows That She Can Love Other People
+        whatIsFate.main();      //  This shows What Romeo and Juliet’s Fate Could Be
+    }
+}
+class iLoveYouRomeo
+{
     public static void main()
-        {
+    {
         System.out.println("  _____   _                                      _____\n" +
                 " |_   _| | |                                    |  __ \\\n" +
                 "   | |   | | _____   _____   _   _  ___  _   _  | |__) |___  _ __ ___   ___  ___\n" +
@@ -20,12 +20,12 @@ class iLoveYouRomeo
                 "                              __/ |\n" +
                 "                             |___/");
         System.out.println("");
-        }
     }
+}
 class iLoveYouQuestion
-    {
+{
     public static void main()
-        {
+    {
         System.out.println("  _____   _                           _          ___  \n" +
                 " |_   _| | |                         | |        |__ \\ \n" +
                 "   | |   | | _____   _____  __      _| |__   ___   ) |\n" +
@@ -33,24 +33,24 @@ class iLoveYouQuestion
                 "  _| |_  | | (_) \\ V /  __/  \\ V  V /| | | | (_) |_|  \n" +
                 " |_____| |_|\\___/ \\_/ \\___|   \\_/\\_/ |_| |_|\\___/(_)  ");
         System.out.println("");
-        }
     }
+}
 class whatIsFate
+{
+    public static void main()
     {
-     public static void main()
-        {
-            System.out.println("FATE: Juliet and Romeo being together from the beginning\n" +
-                    "were going to set them up for bad fate. Towards the\n" +
-                    "beginning, Mercutio was brutally stabbed by Tybalt, which\n" +
-                    "led to his death. Romeo got revenge on Tybalt by dueling\n" +
-                    "with him until his death. Also with Juilet making the bad\n" +
-                    "decision to take the potion to make her appear to be dead,\n" +
-                    "which ultimately caused both of them to die. Therefore Juliet\n" +
-                    "and Romeo's fate was not meant to be together because of\n" +
-                    "all of the borders and boundaries that were constantly\n" +
-                    "surrounding them.");
-        }
+        System.out.println("FATE: Juliet and Romeo being together from the beginning\n" +
+                "were going to set them up for bad fate. Towards the\n" +
+                "beginning, Mercutio was brutally stabbed by Tybalt, which\n" +
+                "led to his death. Romeo got revenge on Tybalt by dueling\n" +
+                "with him until his death. Also, with Juliet making the bad\n" +
+                "decision to take the potion to make her appear to be dead,\n" +
+                "which ultimately caused both of them to die. Therefore Juliet\n" +
+                "and Romeo's fate was not meant to be together because of\n" +
+                "all of the borders and boundaries that were constantly\n" +
+                "surrounding them.");
     }
+}
 /*
 I used the "BIG" FONT
   _____   _                                      _____
@@ -67,7 +67,7 @@ FATE: Juliet and Romeo being together from the beginning
 were going to set them up for bad fate. Towards the
 beginning, Mercutio was brutally stabbed by Tybalt, which
 led to his death. Romeo got revenge on Tybalt by dueling
-with him until his death. Also with Juilet making the bad
+with him until his death. Also, with Juliet making the bad
 decision to take the potion to make her appear to be dead,
 which ultimately caused both of them to die. Therefore Juliet
 and Romeo's fate was not meant to be together because of
@@ -75,7 +75,7 @@ all of the borders and boundaries that were constantly
 surrounding them.
  */
 /*
-THIS IS THE OUTPUT OF THE PROGRAM::: 
+THIS IS THE OUTPUT OF THE PROGRAM:::
   _____   _                                      _____
  |_   _| | |                                    |  __ \
    | |   | | _____   _____   _   _  ___  _   _  | |__) |___  _ __ ___   ___  ___
@@ -85,21 +85,22 @@ THIS IS THE OUTPUT OF THE PROGRAM:::
                               __/ |
                              |___/
 
-  _____   _                           _          ___  
- |_   _| | |                         | |        |__ \ 
+  _____   _                           _          ___
+ |_   _| | |                         | |        |__ \
    | |   | | _____   _____  __      _| |__   ___   ) |
-   | |   | |/ _ \ \ / / _ \ \ \ /\ / / '_ \ / _ \ / / 
-  _| |_  | | (_) \ V /  __/  \ V  V /| | | | (_) |_|  
- |_____| |_|\___/ \_/ \___|   \_/\_/ |_| |_|\___/(_)  
+   | |   | |/ _ \ \ / / _ \ \ \ /\ / / '_ \ / _ \ / /
+  _| |_  | | (_) \ V /  __/  \ V  V /| | | | (_) |_|
+ |_____| |_|\___/ \_/ \___|   \_/\_/ |_| |_|\___/(_)
 
 FATE: Juliet and Romeo being together from the beginning
 were going to set them up for bad fate. Towards the
 beginning, Mercutio was brutally stabbed by Tybalt, which
 led to his death. Romeo got revenge on Tybalt by dueling
-with him until his death. Also with Juilet making the bad
+with him until his death. Also, with Juliet making the bad
 decision to take the potion to make her appear to be dead,
 which ultimately caused both of them to die. Therefore Juliet
 and Romeo's fate was not meant to be together because of
 all of the borders and boundaries that were constantly
 surrounding them.
  */
+
